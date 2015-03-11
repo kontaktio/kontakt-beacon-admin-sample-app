85a32a391367cde6c6d41b71cc27a30674e8546c
Dawid Gdanski <dawid@kontakt.io>
2015-03-11 16:10:46 +0100

    Adding ButterKnife logging

--- app/src/main/java/com/kontakt/sample/App.java
+++ app/src/main/java/com/kontakt/sample/App.java
@@ -5,14 +5,16 @@ import android.app.Application;
 import com.kontakt.sdk.android.http.KontaktApiClient;
 import com.kontakt.sdk.android.util.Logger;
 
+import butterknife.ButterKnife;
+
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-
         Logger.setDebugLoggingEnabled(BuildConfig.DEBUG);
         Logger.setCrashlyticsLoggingEnabled(true);
         KontaktApiClient.init(this);
+        ButterKnife.setDebug(BuildConfig.DEBUG);
     }
 }
