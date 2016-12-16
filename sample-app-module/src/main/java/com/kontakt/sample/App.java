ffce2baee48ec6f341cf9d20a5e4c9cd0ff4006a
michal <m.drabik@kontakt.io>
2016-12-16 14:35:51 +0100

    Refactor

--- sample-app-module/src/main/java/com/kontakt/sample/App.java
+++ sample-app-module/src/main/java/com/kontakt/sample/App.java
@@ -5,14 +5,16 @@ import com.kontakt.sdk.android.common.KontaktSDK;
 
 public class App extends Application {
 
+  private static final String API_KEY = "Put your API key here";
+
   @Override
   public void onCreate() {
     super.onCreate();
     initializeDependencies();
   }
 
-  //Initializing Kontakt SDK. Insert your API key here to allow all samples to work correctly
+  //Initializing Kontakt SDK. Insert your API key to allow all samples to work correctly
   private void initializeDependencies() {
-    KontaktSDK.initialize("Put your API key here");
+    KontaktSDK.initialize(API_KEY);
   }
 }
